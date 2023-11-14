daa0117a7db9b74028e697a5c872a5389253a460
dhei <yuhei@ie.u-ryukyu.ac.jp>
2023-11-14 14:53:56 +0900

    update vite setting

--- vite.config.ts
+++ vite.config.ts
@@ -16,11 +16,6 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    https: {
-      key: fs.readFileSync('key.pem'),
-      cert: fs.readFileSync('cert.pem'),
-      passphrase: '0000',
-    },
   },
   base: '/webxr/',
   build: {

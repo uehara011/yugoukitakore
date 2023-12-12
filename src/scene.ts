96390df95f684bcb20a49eea2e146bc069f17068
tokumorisky <e205717@ie.u-ryukyu.ac.jp>
2023-12-12 16:21:09 +0900

    add scene

--- src/scene.ts
+++ src/scene.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import useLogger from './logger';
 const log = useLogger();
+import Cannon from 'cannon';
 
 export interface ARScene {
     makeObjectTree(): THREE.Object3D;
@@ -13,84 +14,65 @@ export class TestScene implements ARScene {
     cube?: THREE.Object3D;
 
     name() { return "test"; }
-    makeObjectTree(): THREE.Object3D {
-        // log.info("make object tree", this.name())
-        const geometry = new THREE.BoxGeometry(1, 5, 5).translate(
-            0,
-            0,
-            0
-        );
 
-        const material = new THREE.MeshPhongMaterial({
-            color: 0xffffff,
-        });
+    makeObjectTree(): THREE.Object3D {
+        const geometry = new THREE.BoxGeometry(0.01, 5, 5).translate(0, 0, 0);
+        const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
         const cube = new THREE.Mesh(geometry, material);
-        this.cube = cube
+        this.cube = cube;
         return cube;
     }
 
     animate(sec: number): void {
         if (!this.cube) return;
-
         // 立方体を回転させるアニメーション
-        //this.cube.rotation.x += 0.01;
+        // this.cube.rotation.x += 0.01;
     }
 }
 
-
-
-export async function loadSvgTexture(svgURL: string): Promise<THREE.Texture> {
-    return new Promise((resolve, reject) => {
-      // SVGテクスチャの読み込み処理を実装する
-      // ここでは具体的なロジックを示していませんが、外部のライブラリや方法を使用してSVGをテクスチャとして読み込む処理を記述してください
-      // 例: Three.jsのTextureLoaderを使用する場合
-      const loader = new THREE.TextureLoader();
-      loader.load(
-        svgURL,
-        (texture) => {
-          resolve(texture);
-        },
-        undefined,
-        (error) => {
-          reject(error);
-        }
-      );
-    });
-  }
-  
-
-
-export class TestScene2 implements ARScene {
-    ball?: THREE.Mesh; // TestScene2で使用するボールのオブジェクト
-
-    name() { return "test2"; }
+export class TestScene2 {
+    ball?: THREE.Mesh;
 
     constructor() {
-        this.initBall(); // ボールの初期化
+        this.initBall();
     }
 
     async initBall() {
-        const svgURL = './assets/logo.svg'; // SVGファイルのパスを指定してください
-        const ballRadius = 0.5;
-        const ballGeometry = new THREE.SphereGeometry(ballRadius, 32, 32);
-        const texture = await loadSvgTexture(svgURL); // createBall.tsのloadSvgTextureと同様の処理を行う（詳細は省略）
-
-        const material = new THREE.MeshBasicMaterial({ map: texture });
-        this.ball = new THREE.Mesh(ballGeometry, material); // ボールを直接生成し、TestScene2のボールに代入
+        // ボールの生成（ここでは簡略化）
+        const ballGeometry = new THREE.SphereGeometry(1, 32, 32);
+        const ballMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        this.ball = new THREE.Mesh(ballGeometry, ballMaterial);
     }
 
-    makeObjectTree(): THREE.Object3D {
-        if (!this.ball) throw new Error('Ball not initialized');
-        return this.ball;
-    }
+    launchBallAnimation(world: Cannon.World) {
+        const ball = this.ball;
+        if (!ball) return;
 
+        const ballShape = new Cannon.Sphere(0.5);
+        const ballBody = new Cannon.Body({ mass: 1, shape: ballShape });
+        world.addBody(ballBody);
 
-    animate(sec: number): void {
-        if (!this.ball) return;
+        const initialVelocity = new Cannon.Vec3(0, 0, -5);
 
-        // 立方体を回転させるアニメーション
-        //this.cube.rotation.x += 0.01;
-        
+        const launchBall = () => {
+            ballBody.position.set(0, 0, 0);
+            ballBody.velocity.copy(initialVelocity);
+        };
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+            world.step(1 / 60);
+
+            const cannonPosition = ballBody.position;
+            const position = new THREE.Vector3(cannonPosition.x, cannonPosition.y, cannonPosition.z);
+            ball.position.copy(position);
+        };
+
+        animate();
+
+        launchBall(); // ボールの発射処理を最初に実行
+
+        // ボタンなどのUI要素はこのメソッド内で追加することも可能です
+        // ここでは省略しています
     }
 }
-
